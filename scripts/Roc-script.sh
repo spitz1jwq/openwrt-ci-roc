fba3ff325cf3c4a6106e00ef2679e6c249b8d0a4
spitz <spitz1jwq@gmail.com>
2025-08-07 16:28:05 +0800

    Update Roc-script.sh

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -12,6 +12,7 @@ sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ Built by Roc')/g" feeds/luci/modu
 # 移除要替换的包
 rm -rf feeds/packages/net/open-app-filter
 rm -rf feeds/luci/applications/luci-app-appfilter
+rm -rf feeds/packages/net/daed
 rm -rf feeds/packages/net/adguardhome
 
 # Git稀疏克隆，只克隆指定目录到本地
